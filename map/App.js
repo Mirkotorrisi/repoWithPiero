04df5d0d35bf0e8d7d92a8d1815b408722ce7508
ClaudioALessandroStevejobs <claudio.alessandro@stevejobs.academy>
2020-10-09 12:27:48 +0200

    commit jonny

--- map/App.js
+++ map/App.js
@@ -25,7 +25,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>modifica jonny</Text>
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
