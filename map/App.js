e38b711bcae25ab8a4a6c5e44d4a8416aa088c7d
Mirko Torrisi <mirkotorrisi@MacBook-Pro-di-Carlo.local>
2020-10-09 12:16:29 +0200

    second commit

--- map/App.js
+++ map/App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Linking } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
+
+const handlePress = (pos) => {
+  Linking.openURL(`https://www.google.com/maps/dir/?api=1&destination=${pos.latitude},${pos.longitude}&travelmode=driving`)
+}
+let position = {
+  latitude: 37.5,
+  longitude: 15.09
+}
+let position2 = {
+  latitude: 37.4,
+  longitude: 15.11
+}
+let position3 = {
+  latitude: 37.1,
+  longitude: 15.08
+}
 export default function App() {
-  const handlePress = (coord) => console.log(coord)
-  let coordinates = {
-    latitude: 40.7143528,
-    longitude: -74.0059731
-  }
+
+
 
   return (
     <View style={styles.container}>
@@ -17,27 +30,34 @@ export default function App() {
         provider={PROVIDER_GOOGLE}
         style={styles.map}
         region={{
-          latitude: 40.7143528,
-          longitude: -74.0059731,
+          latitude: 37.5,
+          longitude: 15.09,
           latitudeDelta: 0.01,
           longitudeDelta: 0.01,
         }}
       >
         <Marker
           key={1}
-          coordinate={coordinates}
+          coordinate={position}
           title={"marker.title"}
           description={"marker.description"}
-          onPress={handlePress(coordinates)}
+          onPress={(position) => handlePress(position)}
         />
         <Marker
           key={2}
-          coordinate={{ latitude: 40.7543528, longitude: - 74.1059731 }}
+          coordinate={position2}
           title={"marker.title"}
           description={"marker.description"}
-          onPress={handlePress}
-
+          onPress={(position2) => handlePress(position2)}
         />
+        <Marker
+          key={3}
+          coordinate={position3}
+          title={"marker.title"}
+          description={"marker.description"}
+          onPress={(position3) => handlePress(position3)}
+        />
+
       </MapView>
     </View>
   );
